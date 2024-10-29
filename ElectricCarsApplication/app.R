14c20cf54b3e578bf5ac6186b133cb3e22906d7f
slkelly22 <slkelly@go.olemiss.edu>
2024-10-29 10:29:37 -0500

    added text to the EV app; also deployed the app to Shiny.io

--- ElectricCarsApplication/app.R
+++ ElectricCarsApplication/app.R
@@ -8,12 +8,18 @@ ev_data <- read_csv("https://data.wa.gov/api/views/f6w7-q2d2/rows.csv?accessType
 
 # ui
 ui <- fluidPage(
-  titlePanel("Let's review this electric vehicle data"),
+  titlePanel("Let's review electric vehicle data from Washington state"),
+  radioButtons("car", 
+               h4("Do you have an electric vehicle?"), choices = list("do", "do not", "do not yet", "never will"), selected = character(0)),
+  textOutput("EVstatus"),
   dataTableOutput("dynamic")
 )
 
 # server
 server <- function(input, output){
+  output$EVstatus <- renderText({
+    paste("You", input$car, "have an electric vehicle.")
+  })
   output$dynamic <- renderDataTable(ev_data)
 }
 
